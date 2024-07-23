ed7012e227a8cae57426135ff24fabe828a54d73
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2024-07-22 20:50:47 -0700

    Refine units list and accommodate different words

--- main.py
+++ main.py
@@ -9,21 +9,24 @@ with open("reverse.json", "r") as f:
 with open("res.json", "r") as f:
     raw_data = json.loads(f.read())
 
-query = " " + input(">>> ").upper().replace(" ", "")
+q = "".join(filter(lambda x: x.isalpha() or x == " ", input(">>> ").upper()))
 
 @functools.lru_cache()
-def recurse(n):
+def recurse(query, n):
     if n <= 0:
         return []
     res = []
     for i in range(5):
         if query[n - i:n + 1] in data:
-            res.append(recurse(n - i - 1) + [random.choice(data[query[n - i:n + 1]])])
+            res.append(recurse(query, n - i - 1) + [random.choice(data[query[n - i:n + 1]])])
     if not res:
         return []
     return res[-1]
 
-res = recurse(len(query) - 1)
+for word in q.split(" "):
+    res = recurse(" " + word, len(word))
 
-for unit in res:
-    print(raw_data[unit]["abbrev"], "\t", unit)
+    for unit in res:
+        print(raw_data[unit]["abbrev"], "\t", unit)
+
+    print()
